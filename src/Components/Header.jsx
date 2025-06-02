e3ce570f1e58db772fd8572d7c699ce83ec87f0e
sm praneeth kumar <smparneethkumr431gmail.com>
2025-06-02 20:45:26 +0530

    header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,20 +1,33 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import achieves from "../assets/achievers_logo.png"
+import { MdAccountCircle } from "react-icons/md";
+import { IoMdCart } from 'react-icons/io';
 
 const Header = () => {
     return (
         <div>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+                        <img src={achieves} alt="" ></img>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
                             <li className="nav-item">
-                                <Link to="/" className="nav-link active" aria-current="page" href="#">Home</Link>
+                                <Link to="/" className="nav-link active" aria-current="page" >Home</Link>
                             </li>
+                            <li className="nav-item">
+                                <Link to="/Shop" className="nav-link active" aria-current="page" >Shop</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/cart" className="nav-link active" aria-current="page" >Cart</Link>
+                            </li>
+                            <li className="nav-item">
+                                <MdAccountCircle />
+                            </li>
+                            <li className="nav-item">
+                               <IoMdCart />
+                            </li>
+                              
                         </ul>
                     </div>
                 </div>
--- src/Routes/Routing.jsx
+++ src/Routes/Routing.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../Components/Home'
+import Shop from '../Components/Shop'
+import Cart from '../Components/Cart'
 
 export class Routing extends Component {
   render() {
     return (
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/shop' element={<Shop/>}/>
+        <Route path='/cart' element={<Cart/>}/>
       </Routes>
     )
   }
